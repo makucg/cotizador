de894d14285786e006c212b87745e53bab3ddc9a
makucg <chengokuu@gmail.com>
2021-05-12 00:22:11 +0200

    Validación formulario

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import Header from './components/Header';
+import styled from '@emotion/styled';
+import Formulario from './components/Formulario';
+
+const Cotenedor = styled.div`
+  max-width: 600px;
+  margin: auto;
+`;
+
+const CotenedorFormulario = styled.div`
+  background-color: #FFFFFF;
+  padding: 3rem;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <Cotenedor className="App">
       <Header 
         titulo='Cotizador de seguros'
       />
-    </div>
+      
+      <CotenedorFormulario>
+        <Formulario />
+      </CotenedorFormulario>
+    </Cotenedor>
   );
 }
 
---
+++ src/components/Formulario.js
@@ -0,0 +1,154 @@
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
+
+const Campo = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+  align-items: center;
+`;
+
+const Label = styled.label`
+  flex: 0 0 100px;
+`;
+
+const Select = styled.select`
+  display: block;
+  width: 100%;
+  padding: 1rem;
+  border: 1px solid #e1e1e1;
+  -webkit-appearance: none;
+`;
+
+const InputRadio = styled.input`
+  margin: 0 1rem;
+`;
+
+const Boton = styled.button`
+  background-color: #00838F;
+  font-size: 16px;
+  width: 100%;
+  padding: 1rem;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: bold;
+  border: none;
+  transition: background-color .3s ease;
+  margin-top: 2rem;
+  &:hover {
+      background-color: #26C6DA;
+      cursor: pointer;
+  }
+`;
+
+const Error = styled.div`
+    background-color: red;
+    color: white;
+    padding: 1rem;
+    width: 100%;
+    text-align: center;
+    margin-bottom: 2rem;
+`;
+
+const Formulario = () => {
+
+    //Creamos state
+    const [datos, guardarDatos] = useState({
+        marca: '',
+        year: '',
+        plan: ''
+    });
+
+    const [error, guardarError] = useState(false);
+
+    //Extraemos valores de un state
+    const {marca, year, plan} = datos;
+
+    //Obtener información del formulario
+    const obtnerInformacion = (e) => {
+        guardarDatos({
+            ...datos,
+            [e.target.name]: e.target.value
+        });
+    }
+    
+    const cotizarSeguro = (e) => {
+        e.preventDefault();
+
+        //validamos datos
+        if (marca === '' || year === '' || plan === '') {
+            guardarError(true);
+            return;
+        }
+
+        guardarError(false);
+
+
+
+    }
+
+    return ( 
+        <form
+            onSubmit={cotizarSeguro}
+        >
+            {
+                error 
+                ? <Error>Todos los campos son obligatorios</Error>
+                : null
+            }
+            <Campo>
+                <Label>Marca</Label>
+                <Select
+                    name="marca"
+                    value={marca}
+                    onChange={obtnerInformacion}
+                >
+                    <option value="">-- Seleccione --</option>
+                    <option value="americano">Americano</option>
+                    <option value="europeo">Europeo</option>
+                    <option value="asiatico">Asiático</option>
+                </Select>
+            </Campo>
+            <Campo>
+                <Label>Año</Label>
+                <Select
+                    name="year"
+                    value={year}
+                    onChange={obtnerInformacion}
+                >
+                    <option value="">-- Seleccione --</option>
+                    <option value="2021">2021</option>
+                    <option value="2020">2020</option>
+                    <option value="2019">2019</option>
+                    <option value="2018">2018</option>
+                    <option value="2017">2017</option>
+                    <option value="2016">2016</option>
+                    <option value="2015">2015</option>
+                    <option value="2014">2014</option>
+                    <option value="2013">2013</option>
+                    <option value="2012">2012</option>
+                </Select>
+            </Campo>
+            <Campo>
+                <Label>Plan</Label>
+                <InputRadio
+                    type="radio"
+                    name="plan"
+                    value="basico"
+                    checked={plan === "basico"}
+                    onChange={obtnerInformacion}
+                /> Básico
+                <InputRadio
+                    type="radio"
+                    name="plan"
+                    value="completo"
+                    checked={plan === "completo"}
+                    onChange={obtnerInformacion}
+                /> Completo
+            </Campo>
+
+            <Boton type="submit">Cotizar</Boton>
+        </form>
+     );
+}
+ 
+export default Formulario;
